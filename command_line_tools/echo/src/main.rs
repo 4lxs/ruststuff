e7f1b457a23a2ab03ff52182bca628328a95f663
Svl <svensek.luka@pm.me>
2024-03-07 20:26:08 +0100

    echo

--- command_line_tools/echo/src/main.rs
+++ command_line_tools/echo/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'n')]
+    no_newline: bool,
+
+    args: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    print!(
+        "{}{}",
+        cli.args.join(" "),
+        if cli.no_newline { "" } else { "\n" }
+    );
 }
